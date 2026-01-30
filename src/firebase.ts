b97fd8b0e9fac737fb586211eb3f966baa8b0838
soohnza9 <soohnza9@users.noreply.github.com>
2026-01-30 16:35:27 +0900

    chore: move firebase config to env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxNWXfDKR8AebCRK14Zl_2Zlb6Rn5bn8w",
-  authDomain: "noggang-studio.firebaseapp.com",
-  projectId: "noggang-studio",
-  storageBucket: "noggang-studio.firebasestorage.app",
-  messagingSenderId: "710393502326",
-  appId: "1:710393502326:web:c84fca400ba781949e9d57"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
