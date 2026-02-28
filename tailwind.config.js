1a4fdfc0685d8f4367f7a5ea26e2aa8f428bd526
soohnza9 <soohnza9@users.noreply.github.com>
2026-02-28 15:38:08 +0900

    수정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.{js,ts,jsx,tsx}",
+    "./index.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./api/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
